88c63f5fadcdc8e099e24fc4c153911bf10acfda
Munis Azatov <90086392+Sinumi@users.noreply.github.com>
2024-05-22 14:54:12 +0500

    feat: Add Gemini module and controller (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CoursesModule } from './courses/courses.module';
+import { GeminiModule } from './gemini/gemini.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { ModulesModule } from './modules/modules.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { ModulesModule } from './modules/modules.module';
-import { LessonsModule } from './lessons/lessons.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, ModulesModule, LessonsModule, CoursesModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ModulesModule,
+    LessonsModule,
+    CoursesModule,
+    GeminiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
