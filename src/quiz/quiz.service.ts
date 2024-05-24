ae8e8ef02b56eb454041c2ef0c7dd8b741992bf4
Zilola <44432264+zilola08@users.noreply.github.com>
2024-05-24 14:25:53 +0500

    refactor: quiz service

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -11,22 +11,24 @@ export class QuizService {
 
   async generateQuizQuestions(lessonId: string) {
     const lesson = await this.lessonsService.getLessonById(lessonId);
-    const prompt = `Generate a quiz with 5 open-ended questions of varying difficulty levels based on ${lesson.content}. Return a list of questions formatted exactly like this: {question: question answer: answer}---{question: question = answer: answer}. Do not use line break anywhere in your response.`;
+    const prompt = `Generate a quiz with 5 open-ended questions of varying difficulty levels based on ${lesson.content}. Return a list of questions formatted exactly like this: {question: question answer: answer}---{question: question answer: answer}. Do not use line break anywhere in your response.`;
     let response;
     try {
       response = await this.geminiService.generateContent(prompt);
     } catch (error) {
       throw new Error('Error generating content with AI');
     }
-    const stringArray = response.response.candidates[0].content.parts[0].text.split('---');
-    console.log("stringArray", stringArray);
-    const objectArray = stringArray.map(item => {
+    const quizResponse = response.response.candidates[0].content.parts[0].text.split('---');
+    if (!quizResponse) {
+      throw new Error('Error generating quiz questions');
+    };
+    const quizObject = quizResponse.map(item => {
       const properties = item.split('answer:');
       return {
         question: properties[0].replace('{', '').replace('question:', '').trim(),
         answer: properties[1].replace('}', '').trim()
       };
     });
-    return objectArray;
+    return quizObject;
   }
 }
