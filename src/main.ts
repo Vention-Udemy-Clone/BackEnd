514d12350b1e6250971fd33133008364023c1ad2
Munis Azatov <iMoonis138@gmail.com>
2024-06-10 22:48:36 +0500

    fix origin bug

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:4173',
-      'https://frontend-iinx.onrender.com/',
-    ],
+    origin: true,
     credentials: true,
   });
   app.useGlobalFilters(new GlobalExceptionFilter());
